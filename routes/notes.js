18d54202a6965ecf84444525ec2dab3b61284aef
WGT <williamgeorgethomas@gmail.com>
2023-01-12 22:04:40 -0600

    added delete function to notes

--- routes/notes.js
+++ routes/notes.js
@@ -1,11 +1,10 @@
 const notes = require("express").Router();
-const { readFromFile, readAndAppend } = require("../helpers/fsUtils");
+const { readFromFile, readAndAppend, writeToFile } = require("../helpers/fsUtils");
 const { v4: uuidv4 } = require("uuid");
 
 notes.get("/", (req, res) => {
   readFromFile("./db/db.json").then((data) => res.json(JSON.parse(data)));
 });
-
 
 notes.post("/", (req, res) => {
   console.log(req.body);
@@ -25,5 +24,21 @@ notes.post("/", (req, res) => {
     res.error("Error in adding note");
   }
 });
+
+
+
+notes.delete("/:id", (req, res) => {
+  const noteId = req.params.id;
+  readFromFile("./db/db.json")
+    .then((data) => JSON.parse(data))
+    .then((notes) => {
+      console.log(notes);
+      let filteredNotes = notes.filter((note) => note.id !== noteId);
+      writeToFile("./db/db.json", filteredNotes);
+      res.json(`Note deleted successfully 🚀`);
+    });
+});
+
+
 
 module.exports = notes;
